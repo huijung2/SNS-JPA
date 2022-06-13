0b888604a38b1b93a5c279af973332f1492e9a92
huijung <huijung01@naver.com>
2022-06-13 20:09:31 +0900

    회원가입 전처리

--- Photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
+++ Photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,8 +1,15 @@
 package com.cos.photogramstart.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -39,8 +46,14 @@ public class AuthController{
 
 	// 회원가입버튼 -> .auth/signup -> /auth/signin
 	@PostMapping("/auth/signup")
-	public String signup(SignupDto signupDto) {
-		log.info(signupDto.toString());
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for(FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+		}
 		// DTO에서 지정한 toEntitiy가 넘어오도록 설정
 		User user = signupDto.toEntitiy();
 		User userEntity = authService.회원가입(user);
--- Photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ Photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,14 +1,21 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
 
 @Data //Getter, Setter를 만들어주는 어노테이션
 public class SignupDto {
+	@Max(20)
 	private String username;
+	@NotBlank
 	private String password;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String name;
 	
 	public User toEntitiy() {
